2248440d8a1b66363cc390c46ec64cd4a5d04118
Robin <robinnem@outlook.com>
2026-04-09 05:58:11 -0400

    Top Sellers section

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTopSellers = async () => {
+      try {
+        const response = await fetch(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers",
+        );
+        const data = await response.json();
+        setSellers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching top sellers:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +35,64 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading ? (
+                // --- SKELETON LOADING STATE ---
+                // Maps 12 skeletons for the list of top 12 sellers
+                new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          borderRadius: "50%",
+                        }}
+                      ></div>
+                    </div>
+                    <div className="author_list_info">
+                      <div
+                        className="skeleton-box"
+                        style={{
+                          width: "100px",
+                          height: "20px",
+                          marginBottom: "4px",
+                        }}
+                      ></div>
+                      <div
+                        className="skeleton-box"
+                        style={{ width: "60px", height: "15px" }}
+                      ></div>
+                    </div>
+                  </li>
+                ))
+              ) : sellers.length > 0 ? (
+                // --- ACTUAL DATA STATE ---
+                sellers.map((seller) => (
+                  <li key={seller.id}>
+                    <div className="author_list_pp">
+                      {/* Link to the specific author's profile page */}
+                      <Link to={`/author/${seller.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={seller.authorImage}
+                          alt={seller.authorName}
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${seller.authorId}`}>
+                        {seller.authorName}
+                      </Link>
+                      <span>{seller.price} ETH</span>
+                    </div>
+                  </li>
+                ))
+              ) : (
+                // --- EMPTY STATE ---
+                <div className="text-center">No top sellers found.</div>
+              )}
             </ol>
           </div>
         </div>
